b25239f34d68f72ad6087a91ceeb570215d3f10a
ruchitiwari20012 <67836089+ruchitiwari20012@users.noreply.github.com>
2022-09-26 17:32:57 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,20 +1,13 @@
 from django.urls import path, include
-from django.conf import settings
 from tracker_api import views
-from django.conf.urls.static import static
-#from .views import (UserApiView)
+
+
 
 
 urlpatterns = [
-    #path('api-auth/', include('rest_framework.urls')),
-    path('login_mobile/', views.user_list),
+    path('login/', views.login),
     path('user_detail/', views.user_detail_view),
-    path('user_detail/:id', views.user_detail_view),
-
-
-    #path('tracker_api/published', views.user_list_published),
-    #path('api', UserApiView.as_view()),
+    #path('token/', views.token_compare)
     ]
-
 
 
