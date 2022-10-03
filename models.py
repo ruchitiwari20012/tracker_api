0e059156ac772fe059f36bc96668a8d12befb805
ruchitiwari20012 <67836089+ruchitiwari20012@users.noreply.github.com>
2022-10-03 17:45:24 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
+from cgi import MiniFieldStorage
+from unittest.util import _MAX_LENGTH
 from django.db import models
-from django.contrib.auth.models import User
 import uuid
 
 # Create your models here.
@@ -20,7 +21,6 @@ class User(models.Model):
 class Organisation(models.Model):
     id = models.UUIDField(primary_key= True,default= uuid.uuid4,editable= False)
     name = models.CharField(max_length=50)
-    truks = models.CharField(max_length=50)
 
     class Meta:
         managed = True
@@ -37,3 +37,82 @@ class AccessToken(models.Model):
     class Meta:
         managed = True
         db_table = 'access_token'
+
+class TruckManufacturer(models.Model):
+    id = models.UUIDField(primary_key= True,default= uuid.uuid4,editable= False)
+    name = models.CharField(max_length=50)
+    manufactured_date = models.DateField(blank=True, null=True)
+    truck_number = models.CharField(max_length=50)
+    model_number = models.CharField(max_length=50)
+    vehicle_class = models.CharField(max_length=50)
+    vehicle_type = models.CharField(max_length=50)
+    alerts = models.CharField(max_length=50)
+
+    class Meta:
+        managed = True
+        db_table = 'truck_manufacturer'
+
+class TruckEmissionNorms(models.Model):
+    id = models.UUIDField(primary_key= True,default= uuid.uuid4,editable= False)
+    co = models.CharField(max_length=50)
+    co_corrected = models.CharField(max_length=50)
+    hc = models.CharField(max_length=50)
+    co2 = models.CharField(max_length=50)
+    o2 = models.CharField(max_length=50)
+    rpm = models.CharField(max_length=50)
+    oil_temperature = models.CharField(max_length=50)
+
+    class Meta:
+        managed = True
+        db_table = 'truck_emission_norms'
+
+class TruckHealthStatus(models.Model):
+    id = models.UUIDField(primary_key= True,default= uuid.uuid4,editable= False)
+    status = models.CharField(max_length=50)
+    last_service = models.CharField(max_length=50)
+    service_station = models.CharField(max_length=50)
+    service_contact = models.CharField(max_length=50)
+
+    class Meta:
+        managed = True
+        db_table = 'truck_health_status'
+
+class TruckCapacity(models.Model):
+    id = models.UUIDField(primary_key= True,default= uuid.uuid4,editable= False)
+    unladen_weight = models.CharField(max_length=50)
+    horse_power = models.CharField(max_length=50)
+    wheel_base = models.CharField(max_length=50)
+    cylinders = models.CharField(max_length=50)
+    fuel = models.CharField(max_length=50)
+    mileage = models.CharField(max_length=50)
+    size = models.CharField(max_length=50)
+
+    class Meta:
+        managed = True
+        db_table = 'truck_capacity'
+    
+
+class Truck(models.Model):
+    id = models.UUIDField(primary_key= True,default= uuid.uuid4,editable= False)
+    registration_number = models.CharField(max_length=50)
+    vin = models.CharField(max_length=50)
+    manufacturing_info = models.ForeignKey(TruckManufacturer, models.CASCADE, db_column='manufacturing_info',null=True)
+    emission_norms = models.ForeignKey(TruckEmissionNorms, models.CASCADE, db_column='emission_norms',null=True)
+    health_status = models.ForeignKey(TruckHealthStatus, models.CASCADE, db_column='health_status',null=True)
+    capacity = models.ForeignKey(TruckCapacity, models.CASCADE, db_column='capacity',null=True)
+    rc_status = models.CharField(max_length=50)
+    insurance_end_date = models.DateField(blank=True, null=True)
+    organisation = models.ForeignKey(Organisation, models.DO_NOTHING, db_column='organisation',null=True) 
+
+    class Meta:
+        managed = True
+        db_table = 'truck'
+
+class FeedBack(models.Model):
+    
+    feedback = models.CharField(max_length = 500)
+    user_feedback = models.ForeignKey(User, models.DO_NOTHING,db_column='user')
+
+    class Meta:
+        managed = True
+        db_table = 'FeedBack'
