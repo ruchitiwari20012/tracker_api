867bf0455075af68a6318bedf95da24a81771a4d
ruchitiwari20012 <67836089+ruchitiwari20012@users.noreply.github.com>
2022-10-03 17:47:52 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -7,7 +7,9 @@ from tracker_api import views
 urlpatterns = [
     path('login/', views.login),
     path('user_detail/', views.user_detail_view),
-    #path('token/', views.token_compare)
+    path('truck_details/',views.TruckDetails.as_view()),
+    path('truck_list/',views.TruckList.as_view()),
+    path('organisation_details/',views.OrganisationDetails.as_view()),
+    path('organisation_list/',views.OrganisationList.as_view()),
+    path('user_feedback/',views.FeedBackDetail.as_view())
     ]
-
-
